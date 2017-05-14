91ee8afdb954381cfd565b510b3168ba282e49aa
Travis Yoder <trayoda@gmail.com>
2017-05-13 20:44:00 -0700

    make gun class strip apostrophes from titles
    - adds more tests

--- spec/gunSpec.js
+++ spec/gunSpec.js
@@ -9,8 +9,7 @@ describe('Gun', function() {
     Type: 'Burst',
     'Magazine Size': '30 (machine gun) 3 (grenade launcher)',
     'Ammo Capacity': '500',
-    Damage: 'Gun: 5.5 Grenade: 10 (impact) + 25 (explosion)',
-    'Fire Rate': ' 0.10',
+    Damage: 'Gun: 5.5 Grenade: 10 (impact) + 25 (explosion)', 'Fire Rate': ' 0.10',
     'Reload Time': '1.2',
     'Shot Speed': '23',
     Range: 'Infinity.png',
@@ -22,19 +21,19 @@ describe('Gun', function() {
   it('has a name', function() {
     gun = new Gun(data);
 
-    expect(gun.name).toBe('m16')
+    expect(gun.name).toEqual('m16')
   })
 
   it('has a quality', function() {
     gun = new Gun(data);
 
-    expect(gun.quality).toBe('A')
+    expect(gun.quality).toEqual('A')
   })
 
   it('has a damage', function() {
     gun = new Gun(data);
 
-    expect(gun.damage).toBe('gun: 5.5 grenade: 10 (impact) + 25 (explosion)')
+    expect(gun.damage).toEqual('gun: 5.5 grenade: 10 (impact) + 25 (explosion)')
   })
 
   it('accepts damage as a number', function() {
@@ -42,7 +41,7 @@ describe('Gun', function() {
 
     gun = new Gun(data);
 
-    expect(gun.damage).toBe('22')
+    expect(gun.damage).toEqual('22')
   });
 
   it('has notes', function() {
@@ -54,18 +53,24 @@ describe('Gun', function() {
   it('changes empty quality to not found', function() {
    gun = new Gun({})
 
-   expect(gun.quality).toBe('not found')
+   expect(gun.quality).toEqual('not found')
   });
 
   it('changes empty damage to not found', function() {
    gun = new Gun({})
 
-   expect(gun.damage).toBe('not found')
+   expect(gun.damage).toEqual('not found')
   });
 
   it('changes empty notes to not found', function() {
    gun = new Gun({})
 
-   expect(gun.notes).toBe('not found')
+   expect(gun.notes).toEqual('not found')
+  });
+
+  it('removes apostrophes from names', function() {
+   gun = new Gun({Name: 'Robot\'s Left Hand'})
+
+   expect(gun.name).toEqual('robots left hand')
   });
 })
