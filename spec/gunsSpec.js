42d1d08a9baa0584a94c61099e7ea6af04ed8078
Travis Yoder <trayoda@gmail.com>
2017-05-13 20:44:55 -0700

    add guns class and tests

---
+++ spec/gunsSpec.js
@@ -0,0 +1,20 @@
+describe('Gun', function() {
+  var Guns = require('../lib/guns')
+  var guns;
+
+  beforeEach(function() {
+    guns = new Guns();
+  });
+
+  it('can find a gun', function() {
+    gun = guns.find_by_name('rad gun')
+
+    expect(gun.name).toEqual('rad gun')
+  });
+
+  it('can find a different gun', function() {
+    gun = guns.find_by_name('dart gun')
+
+    expect(gun.name).toEqual('dart gun')
+  });
+})
