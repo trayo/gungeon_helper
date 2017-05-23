5be3c58d98b9e90a8eb6c728eed56c174182aba8
Travis Yoder <trayoda@gmail.com>
2017-05-22 21:02:22 -0700

    add not found error message

--- index.js
+++ index.js
@@ -9,28 +9,44 @@ var handlers = {
   'GetGunInformationIntent': function () {
     var gun = guns.find_by_name(this.event.request.intent.slots.Gun.value);
 
-    var gunInformation = gun.name + '. ' +
-      gun.notes +
-      ' The ' + gun.name + ' has a quality of ' + gun.quality + ',' +
-      ' and the damage is ' + gun.damage + '.'
+    if (gun) {
+      var gunInformation = gun.name + '. ' +
+        gun.notes +
+        ' The ' + gun.name + ' has a quality of ' + gun.quality + ',' +
+        ' and the damage is ' + gun.damage + '.'
 
-    this.emit(':tell', gunInformation);
+      this.emit(':tell', gunInformation);
+    } else {
+      this.emit(':tell', 'NotFoundIntent');
+    }
   },
 
   'GetQualityIntent': function () {
     var gun = guns.find_by_name(this.event.request.intent.slots.Gun.value);
 
-    this.emit(':tell', 'The quality for ' + gun.name + ' is ' + gun.quality + '.');
+    if (gun) {
+      this.emit(':tell', 'The quality for ' + gun.name + ' is ' + gun.quality + '.');
+    } else {
+      this.emit(':tell', 'NotFoundIntent');
+    }
   },
 
   'GetDamageIntent': function () {
     var gun = guns.find_by_name(this.event.request.intent.slots.Gun.value);
 
-    this.emit(':tell', 'The damage for ' + gun.name + ' is ' + gun.damage + '.');
+    if (gun) {
+      this.emit(':tell', 'The damage for ' + gun.name + ' is ' + gun.damage + '.');
+    } else {
+      this.emit(':tell', 'NotFoundIntent');
+    }
+  },
+
+  'NotFoundIntent': function () {
+    this.emit(':tell', 'I didn\'t find the item you asked for.');
   },
 
   'Unhandled': function () {
-    this.emit(':tell', 'Ask for information, quality or damage of a gun',  'Ask for information, quality or damage of a gun');
+    this.emit(':tell', 'Ask for information, quality or damage of an item.',  'Ask for information, quality or damage of an item.');
   }
 };
 
